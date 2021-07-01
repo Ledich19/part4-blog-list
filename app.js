5dc4c8d0a7b90400368918c9f54dcf6802f2193c
Aleksandr <ledich19@gmail.com>
2021-07-01 18:21:29 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const blogsRouter = require('./controllers/blogs')
+const middlewere = require('./utils/middlewere')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+morgan.token('content', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
+
+app.use('/api/blogs', blogsRouter)
+app.use(middlewere.errorHandler)
+
+module.exports = app
